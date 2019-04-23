d0ab5de51f234d0d5e70789a1655aa1959bee20a
Haja Bangura <bangurah44@gmail.com>
2019-04-23 10:44:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // write your code below!
 console.log('Hello, World!');
+console.log('Hello, World!');
+index.js;
+node index.js
+clear ()
 
+console.log("Hello, World!")
+
+
+console.log("Hello, World!");
